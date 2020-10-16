701702b5bfd6b537aa6d7249972303048f044e5d
Connor Imes <cimes@isi.edu>
2020-10-16 10:11:02 -0400

    util: add arange function

--- util.h
+++ util.h
@@ -17,6 +17,17 @@ inline Func linspace_func(Expr start, Expr stop, RDom r) {
     return linspace;
 }
 
+inline Func arange_func(Expr start, Expr stop, Expr step) {
+    Var x{"x"};
+    Expr extent("extent");
+    extent = ConciseCasts::i32(ceil((stop - start) / step));
+    RDom r(0, extent, "r");
+    Func arange("arange");
+    arange(x) = start;
+    arange(r) = start + r * step;
+    return arange;
+}
+
 inline Expr log2f_expr(Expr x) {
     Expr log2 = log(x) / log(Expr(2.0f));
     return log2;
