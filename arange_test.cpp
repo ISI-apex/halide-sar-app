6af40c3162d5de0ffea233aa9230d4cd1f82c6c5
Connor Imes <cimes@isi.edu>
2020-10-19 14:42:55 -0400

    arange: add verification, make into unit test

--- arange_test.cpp
+++ arange_test.cpp
@@ -4,6 +4,7 @@
 #include <Halide.h>
 
 #include "arange.h"
+#include "test.h"
 
 using namespace std;
 using Halide::Runtime::Buffer;
@@ -14,18 +15,28 @@ using Halide::Runtime::Buffer;
 
 #define N ceil((((STOP - START) / STEP)))
 
+static void reference(float *ref) {
+    for (size_t i = 0; i < N; i++) {
+        ref[i] = START + i * STEP;
+    }
+}
+
 int main(int argc, char **argv) {
-    Buffer<float> out((int)N);
+    float ref[(size_t)N] = { 0 };
+    Buffer<float, 1> out((int)N);
     int rv = arange(START, STOP, STEP, out);
 
     // output should be: [ 0, 1.5, 3, 4.5, 6, 7.5, 9, 10.5, 12, 13.5 ]
     if (!rv) {
+        print_1d(out);
+        reference(ref);
         float *obuf = out.begin();
-        cout << "[ " << obuf[0];
-        for (size_t i = 1; i < (size_t)N; i++) {
-            cout << ", " << obuf[i];
+        for (size_t i = 0; i < N; i++) {
+            if (abs(ref[i] - obuf[i]) >= 0.1f) {
+                cerr << "Verification failed at index " << i << endl;
+                return -1;
+            }
         }
-        cout << " ]" << endl;
     }
 
     return rv;
