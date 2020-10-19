ba5c23e6df2b2dd24fb3b50247329a77c41e469e
Connor Imes <cimes@isi.edu>
2020-10-19 14:38:45 -0400

    linspace: add verification, make into unit test

--- linspace_test.cpp
+++ linspace_test.cpp
@@ -3,23 +3,38 @@
 #include <Halide.h>
 
 #include "linspace.h"
+#include "test.h"
 
 using namespace std;
 using Halide::Runtime::Buffer;
 
-#define INPUT_NUM 424
+#define INPUT_NUM 10
+#define START -0.5f
+#define STOP 0.5f
+
+static void reference(float *ref) {
+    float step = (STOP - START) / (INPUT_NUM - 1);
+    for (size_t i = 0; i < INPUT_NUM; i++) {
+        ref[i] = START + i * step;
+    }
+    ref[INPUT_NUM - 1] = STOP;
+}
 
 int main(int argc, char **argv) {
-    Buffer<float> out(INPUT_NUM);
-    int rv = linspace(-0.5f, 0.5f, INPUT_NUM, out);
+    float ref[INPUT_NUM] = { 0 };
+    Buffer<float, 1> out(INPUT_NUM);
+    int rv = linspace(START, STOP, INPUT_NUM, out);
 
     if (!rv) {
+        print_1d(out);
+        reference(ref);
         float *obuf = out.begin();
-        cout << "[ " << obuf[0];
-        for (size_t i = 1; i < INPUT_NUM; i++) {
-            cout << ", " << obuf[i];
+        for (size_t i = 0; i < INPUT_NUM; i++) {
+            if (abs(ref[i] - obuf[i]) >= 0.1f) {
+                cerr << "Verification failed at index " << i << endl;
+                return -1;
+            }
         }
-        cout << " ]" << endl;
     }
 
     return rv;
