d537e8da95183b67b4503bd87e4d944b7bee6918
Connor Imes <cimes@isi.edu>
2020-10-23 15:15:35 -0400

    util: add optional name param to functions that produce Func types

--- util.h
+++ util.h
@@ -11,11 +11,11 @@ inline Expr normalize(Expr val, Expr min, Expr max) {
     return (clamp(val, min, max) - min) / (max - min);
 }
 
-inline Func linspace_func(Expr start, Expr stop, RDom r) {
+inline Func linspace_func(Expr start, Expr stop, RDom r, const std::string &name = "linspace") {
     Var x{"x"};
     Expr step("step");
     step = (stop - start) / (r.x.extent() - Expr(1));
-    Func linspace("linspace");
+    Func linspace(name);
     linspace(x) = start;
     linspace(r) = start + r * step;
     // force "stop" value to avoid floating point deviations
@@ -23,12 +23,12 @@ inline Func linspace_func(Expr start, Expr stop, RDom r) {
     return linspace;
 }
 
-inline Func arange_func(Expr start, Expr stop, Expr step) {
+inline Func arange_func(Expr start, Expr stop, Expr step, const std::string &name = "arange") {
     Var x{"x"};
     Expr extent("extent");
     extent = ConciseCasts::i32(ceil((stop - start) / step));
     RDom r(0, extent, "r");
-    Func arange("arange");
+    Func arange(name);
     arange(x) = start;
     arange(r) = start + r * step;
     return arange;
