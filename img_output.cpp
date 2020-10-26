e0b906c5697e722ff0cd6268f4aa88f9a24c295e
Connor Imes <cimes@isi.edu>
2020-10-26 12:13:35 -0400

    img_output: add img_output_to_dB generator

--- img_output.cpp
+++ img_output.cpp
@@ -1,9 +1,32 @@
 #include <Halide.h>
 
+#include "complexfunc.h"
 #include "signal.h"
 #include "util.h"
 
 using namespace Halide;
+
+class ImgOutputToDBGenerator : public Halide::Generator<ImgOutputToDBGenerator> {
+public:
+    Input<Buffer<double>> img {"img", 3}; // complex input
+    Output<Buffer<double>> out{"out", 2};
+
+    void generate() {
+        Var x{"x"}, y{"y"}, c{"c"};
+
+        Func img_func("img_func");
+        img_func = img;
+        ComplexFunc cimg(c, img_func, "input");
+
+        RDom r(0, img.dim(1).extent(), 0, img.dim(2).extent(), "r");
+        Func m("m");
+        m() = maximum(abs(cimg(r.x, r.y)));
+
+        out(x, y) = Expr(10) * log(abs(cimg(x, y)) / m());
+
+        m.compute_root();
+    }
+};
 
 class ImgOutputU8Generator : public Halide::Generator<ImgOutputU8Generator> {
 public:
@@ -18,4 +41,5 @@ public:
     }
 };
 
+HALIDE_REGISTER_GENERATOR(ImgOutputToDBGenerator, img_output_to_dB)
 HALIDE_REGISTER_GENERATOR(ImgOutputU8Generator, img_output_u8)
