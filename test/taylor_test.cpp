d43eb2f1418a3f2638a1a79467f2b51d5e10cd12
Connor Imes <cimes@isi.edu>
2020-11-09 15:06:33 -0500

    test: taylor: automate verification

--- test/taylor_test.cpp
+++ test/taylor_test.cpp
@@ -2,24 +2,33 @@
 
 #include <Halide.h>
 
+#include "test.h"
 #include "taylor.h"
 
 using namespace std;
 using Halide::Runtime::Buffer;
 
-#define INPUT_NUM 424
+#define INPUT_NUM 12
+
+// TODO: need an actual reference function implementation
+static const double reference[INPUT_NUM] = {
+    0.632594, 0.662359, 0.742204, 0.846779, 0.942881, 1,
+    1, 0.942881, 0.846779, 0.742204, 0.662359, 0.632594
+};
 
 int main(int argc, char **argv) {
-    Buffer<double> out(INPUT_NUM);
+    Buffer<double, 1> out(INPUT_NUM);
     int rv = taylor(INPUT_NUM, 17, out);
 
     if (!rv) {
+        print_1d(out);
         double *obuf = out.begin();
-        cout << "[ " << obuf[0];
-        for (size_t i = 1; i < INPUT_NUM; i++) {
-            cout << ", " << obuf[i];
+        for (size_t i = 0; i < INPUT_NUM; i++) {
+            if (abs(reference[i] - obuf[i]) >= 0.0001f) {
+                cerr << "Verification failed at index " << i << endl;
+                return -1;
+            }
         }
-        cout << " ]" << endl;
     }
 
     return rv;
