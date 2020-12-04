4db34261d4215a1c5f870bd5c3baa72171c3a48e
Connor Imes <cimes@isi.edu>
2020-12-04 13:18:21 -0500

    sarbp: silence call_dft debug output

--- sarbp.cpp
+++ sarbp.cpp
@@ -68,7 +68,9 @@ extern "C" DLLEXPORT int call_dft(halide_buffer_t *in, int N_fft, halide_buffer_
     assert(out->dim[1].min == 0);
     assert(out->dim[1].extent == N_fft);
     if(in->is_bounds_query()) {
+#if 0
         cout << "call_dft: bounds query" << endl;
+#endif
         // input and output shapes are the same
         for (int i = 0; i < in->dimensions; i++) {
             in->dim[i].min = out->dim[i].min;
@@ -79,14 +81,18 @@ extern "C" DLLEXPORT int call_dft(halide_buffer_t *in, int N_fft, halide_buffer_
         assert(in->type == halide_type_of<double>());
         assert(out->host);
         assert(fft_plan);
+#if 0
         cout << "call_dft: FFTW: processing vectors ["
              << out->dim[2].min << ", " << out->dim[2].min + out->dim[2].extent << ")" << endl;
+#endif
         fftw_complex *fft_in = (fftw_complex*)in->host;
         fftw_complex *fft_out = (fftw_complex*)out->host;
         for (int i = 0; i < out->dim[2].extent; i++) {
             fftw_execute_dft(fft_plan, &fft_in[i * N_fft], &fft_out[i * N_fft]);
         }
+#if 0
         cout << "call_dft: FFTW: finished" << endl;
+#endif
     }
     return 0;
 }
