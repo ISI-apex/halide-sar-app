381e4cceb9ec578971f4864b6000140f3f17394f
Connor Imes <cimes@isi.edu>
2021-02-12 09:58:51 -0500

    img_output: vectorized/parallel schedule for img_output_to_dB
    Default schedule was starting to take a non-trivial amount of time at
    larger data sizes.

--- img_output.cpp
+++ img_output.cpp
@@ -9,24 +9,33 @@ using namespace Halide::Tools;
 
 class ImgOutputToDBGenerator : public Halide::Generator<ImgOutputToDBGenerator> {
 public:
+    GeneratorParam<int32_t> vectorsize {"vectorsize", 4};
+    GeneratorParam<int32_t> blocksize {"blocksize", 64};
+
     Input<Buffer<double>> img {"img", 3}; // complex input
     Output<Buffer<double>> out{"out", 2};
 
     void generate() {
-        Var x{"x"}, y{"y"}, c{"c"};
-
         Func img_func("cimg");
         img_func = img;
         ComplexFunc cimg(c, img_func);
 
         RDom r(0, img.dim(1).extent(), 0, img.dim(2).extent(), "r");
-        Func m("m");
         m() = maximum(abs(cimg(r.x, r.y)));
 
         out(x, y) = Expr(10) * log10(abs(cimg(x, y)) / m());
-
-        m.compute_root();
     }
+
+    void schedule() {
+        m.compute_root();
+        out.compute_root()
+           .vectorize(x, vectorsize)
+           .parallel(y, blocksize);
+    }
+
+private:
+    Var x{"x"}, y{"y"}, c{"c"};
+    Func m{"m"};
 };
 
 class ImgOutputU8Generator : public Halide::Generator<ImgOutputU8Generator> {
